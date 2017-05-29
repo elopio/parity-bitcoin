35e891b3b374511d06fffdc6e1c780998c303f5d
Travis CI User <travis@example.org>
2017-05-29 15:06:49 +0000

    Update documentation

---
+++ implementors/rand/trait.Rng.js
@@ -0,0 +1,22 @@
+(function() {var implementors = {};
+implementors["bitcrypto"] = [];
+implementors["chain"] = [];
+implementors["db"] = [];
+implementors["import"] = [];
+implementors["keys"] = [];
+implementors["message"] = [];
+implementors["network"] = [];
+implementors["p2p"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/rand/rand/trait.Rng.html\" title=\"trait rand::Rng\">Rng</a> for Fortuna",];
+implementors["primitives"] = [];
+implementors["rpc"] = [];
+implementors["script"] = [];
+implementors["serialization"] = [];
+implementors["verification"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
